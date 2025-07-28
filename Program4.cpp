0a95fa2fe8faee958c2a2c89d2bf24815ee2a399
Shanaysaha <shanaysaha10@gmail.com>
2025-07-28 10:46:56 +0530

    Program4.cpp

--- Program4.cpp
+++ Program4.cpp
@@ -1,19 +1,18 @@
 // Write a program to create a matrix of size 3x2 having all the even numbers. In such a way that numbers in a second row should be double of numbers in a first row.
-#include <iostream>
-using namespace std;
 int main() {
-    int matrix[3][2];
+    int matrix[2][3];
     int num = 2;
-    for (int i = 0; i < 3; i++) {
-        matrix[i][0] = num;
+    for (int j = 0; j < 3; j++) {
+        matrix[0][j] = num;
         num += 2;
     }
-    for (int i = 0; i < 3; i++) {
-        matrix[i][1] = matrix[i][0] * 2;
+    for (int j = 0; j < 3; j++) {
+        matrix[1][j] = matrix[0][j] * 2;
     }
+
     cout << "The matrix is:\n";
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
